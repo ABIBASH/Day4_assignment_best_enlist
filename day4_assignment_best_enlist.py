ee8855b9039a89e90ddb8d7cff55563b4f46525f
ABIBASH <85815699+ABIBASH@users.noreply.github.com>
2021-06-16 07:45:28 +0530

    Add files via upload

---
+++ day4_assignment_best_enlist.py
@@ -0,0 +1,21 @@
+#Creating three variables (a,b,c) to same value of integer
+a=b=c=200
+#divide a by 10
+print(a/10)
+#Multiply b by 50
+print(b*50)
+#Add c by 60
+print(c+60)
+
+#creating a string variable
+string="abcde"
+#Replacing the third character with G
+print(string.replace(string[2],"G"))
+
+#creating two variables (a,b) of data type int and float and coverting them vice versa
+a=10
+b=10.5
+print(float(a))
+print(int(b))
+
+
